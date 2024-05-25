343882817fe5cdd97f20386233874bee1b26ea87
Mohit Jangra <mohitjangra12@gmail.com>
2024-05-26 01:56:29 +0530

    arandr: updated refresh rate for the external monitor

--- dot_screenlayout/private_executable_docked.sh
+++ dot_screenlayout/private_executable_docked.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-3 --off
+xrandr --output eDP-1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP-1 --off --output DP-2 --primary --mode 2560x1440 --rate 74.97 --pos 0x0 --rotate normal --output DP-3 --off
